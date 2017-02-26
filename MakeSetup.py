f4f6edaba12c222c1bc1a4756833f2c27de364f3
unknown <>
2017-02-26 22:47:46 +0800

    Frame OK

--- MakeSetup.py
+++ MakeSetup.py
@@ -62,39 +62,84 @@ def getVCInstallDir(vsVersion):
         return vcInstallDir
     return vcInstallDir
 
+def IsInstallVS(vsVersion):
+    if GetVCInstallDir(vsVersion) != None :
+        return True
+    return False
+
 def CopyDir(dirFrom,dirTo):
-    os.systemp(r'xcopy %s %s /I /E /Y'%(dirFrom,dirTo))
-        
+    os.system(r'xcopy %s %s /I /E /Y'%(dirFrom,dirTo))
+
+def CopyVCToBin(vsVersion):
+        vcInstallDir = None
+    vcVersion = None
+    if vsVersion == VSVersion.VS2005:
+        vcVersion = r'80'
+    elif vsVersion == VSVersion.VS2008:
+        vcVersion = r'90'
+    elif vsVersion == VSVersion.VS2010:
+        vcVersion = r'100'
+    elif vsVersion == VSVersion.VS2012:
+        vcVersion = r'110'
+    elif vsVersion == VSVersion.VS2013:
+        vcVersion = r'110'
+    elif vsVersion == VSVersion.VS2015:
+        vcVersion = r'140'
+
+    vcInstallDir = GetVCInstallDir(vsVersion)
+    if vcInstallDir != None:
+        dirFrom = vcInstallDir + r'\..\..'
+        dirTo = sys.[path0]+r'\Bin\Compiler\v'+vcVersion
+        CopyDir(dirFrom,dirTo)
 
 
-def PrintReg() :
-    try:
-        i = 0
-        while 1:
-            name,value,type = winreg.EnumValue(key,i)
-            print(repr(name),value,type)
-            i+=1
-    except WindowsError:
-        pass
+##def PrintReg() :
+##    try:
+##        i = 0
+##        while 1:
+##            name,value,type = winreg.EnumValue(key,i)
+##            print(repr(name),value,type)
+##            i+=1
+##    except WindowsError:
+##        pass
+##
+##    value,type = winreg.QueryValueEx(key,'Close')
+##    print(value,type)
+##
+##    temp = winreg.QueryValueEx(key,'Close')
+##    print(temp)
 
-    value,type = winreg.QueryValueEx(key,'Close')
-    print(value,type)
-
-    temp = winreg.QueryValueEx(key,'Close')
-    print(temp)
-
-def IsInstallVS():
-    if GetVSRegPath() = None :
-        return False
-    return True
-
-
+def IsInstallVS(vsVersion):
+    if GetVCInstallDir(vsVersion) != None :
+        return True
+    return False
 
 if __name__ == '__main__' :
     #copy VC
-    vcInstallDir = GetVCInstallDir(VSVersion.VS2005)
-    if vcInstallDir != None:
-        vcInstallDir = vcInstallDir + r'\..\..'
-        CopyDir()
-        
+    if IsInstallVS(VSVersion.VS2005)
+        CopyVCToBin(VSVersion.VS2005)
+    elif IsInstallVS(VSVersion.VS2008)
+        CopyVCToBin(VSVersion.VS2008)
+    elif IsInstallVS(VSVersion.VS2010)
+        CopyVCToBin(VSVersion.VS2010)
+    elif IsInstallVS(VSVersion.VS2010)
+        CopyVCToBin(VSVersion.VS2010)
+    elif IsInstallVS(VSVersion.VS2012)
+        CopyVCToBin(VSVersion.VS2012)
+    elif IsInstallVS(VSVersion.VS2013)
+        CopyVCToBin(VSVersion.VS2013)
+    elif IsInstallVS(VSVersion.VS2015)
+        CopyVCToBin(VSVersion.VS2015)
+
+    #copy MicrosoftSDKs
+
+    #copy MSBuild
+
+    #copy WindowsKits
+
+    #download vcredist
+
+    
+
+    
 
