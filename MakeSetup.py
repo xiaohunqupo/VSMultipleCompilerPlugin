236f76ad6eed7a9a7d9a67495a2fa97c8b60e015
xiaohunqupo <xiaohunqupo@163.com>
2017-03-17 13:57:57 +0800

    Fix Bug

--- MakeSetup.py
+++ MakeSetup.py
@@ -12,16 +12,7 @@ def GetPFX86Dir():
         return r'%programfiles(x86)%'
     else:
         return r'%programfiles%'
-    
 
-class VSVersion(enum.Enum):
-    VS2005 = 1
-    VS2008 = 2
-    VS2010 = 3
-    VS2012 = 4
-    VS2013 = 5
-    VS2015 = 6
-    
 ## key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,r'SYSTEM\CurrentControlSet\Services\Tcpip\Performance')
 
 def getHKLMValue(path,name):
@@ -66,132 +57,168 @@ VerList = (
          ('2005', '8.0',  'V80',  '',      '',    '',),
          ('2008', '9.0',  'V90',  'v7.0A', '',    '',),
          ('2010', '10.0', 'V100', 'v7.0A', '',    '',),
-         ('2012', '11.0', 'V110', 'v8.0', '8.0',  'KitsRoot',),
-         ('2013', '12.0', 'V120', 'v8.1', '8.1',  'KitsRoot81',),
-         ('2015', '14.0', 'V140', 'v8.1', '10',   'KitsRoot82',)
+         #('2012', '11.0', 'V110', 'v8.0', '8.0',  'KitsRoot',),
+         ('2012', '11.0', 'V110', '', '8.0',  'KitsRoot',),
+         #('2013', '12.0', 'V120', 'v8.1', '8.1',  'KitsRoot81',),
+         ('2013', '12.0', 'V120', '', '8.1',  'KitsRoot81',),
+         ('2015', '14.0', 'V140', 'v8.1', '10',   'KitsRoot10',)
          )
 
 
-def getVCInstallDir(ver):
-    dir = getHKLMValue(r'SOFTWARE\Microsoft\VisualStudio\%s\Setup\VC'%ver,'ProductDir')
+def getVCInstallDir(VCVer):
+    dir = getHKLMValue(r'SOFTWARE\Microsoft\VisualStudio\{VCV}\Setup\VC'.format(VCV=VCVer),'ProductDir')
     if dir :
-        return dir
-    dir = getHKLMValue(r'SOFTWARE\Wow6432Node\Microsoft\VisualStudio\%s\Setup\VC'%ver,'ProductDir')
+        return dir.rstrip('\\')
+    dir = getHKLMValue(r'SOFTWARE\Wow6432Node\Microsoft\VisualStudio\{VCV}\Setup\VC'.format(VCV=VCVer),'ProductDir')
     if dir :
-        return dir
-    dir = getHKLMValue(r'SOFTWARE\Microsoft\VCExpress\%s\Setup\VC'%ver,'ProductDir')
+        return dir.rstrip('\\')
+    dir = getHKLMValue(r'SOFTWARE\Microsoft\VCExpress\{VCV}\Setup\VC'.format(VCV=VCVer),'ProductDir')
     if dir :
-        return dir
-    dir = getHKLMValue(r'SOFTWARE\Wow6432Node\Microsoft\VCExpress\%s\Setup\VC'%ver,'ProductDir')
+        return dir.rstrip('\\')
+    dir = getHKLMValue(r'SOFTWARE\Wow6432Node\Microsoft\VCExpress\{VCV}\Setup\VC'.format(VCV=VCVer),'ProductDir')
     if dir :
-        return dir
+        return dir.rstrip('\\')
     return None
 
-def isInstallVS(ver):
-    if getVCInstallDir(vsVersion):
+def isInstallVC(VCVer):
+    if getVCInstallDir(VCVer):
         return True
     return False
 
 def copyFile(nameFrom,dirTo):
-    cmd = r'xcopy "{namefrom}" "{dirto}" /Y'.format(namefrom=nameFrom,dirto=dirTo)
+    cmd = r'xcopy "{NF}" "{DT}" /I /Y'.format(NF=nameFrom,DT=dirTo)
     os.system(cmd)
 
 def copyDir(dirFrom,dirTo):
-    cmd = r'xcopy "{dirFrom}" "{dirTo}" /I /E /Y'.format(diurFrom=dirFrom,dirTo=dirTo)
-    #print(cmd)
+    cmd = r'xcopy "{DF}" "{DT}" /I /E /Y'.format(DF=dirFrom,DT=dirTo)
+    print(cmd)
     os.system(cmd)
 
 def copyVC(vcVer,VCDirName):
-    vcDir = getVCInstallDir(ver)
+    vcDir = getVCInstallDir(vcVer)
     if dir :
-        dirFrom = r'{VCPath}\..\Common7'.format(VCPath=vcDir)
-        dirTo = r'{currentPath}\Bin\Compiler\{ver}\Common7'.format(currentPath=sys.path[0],ver=VCDirName)
+        dirFrom = r'{VCD}\..\Common7'.format(VCD=vcDir)
+        dirTo = r'{CP}\Bin\Compiler\{VCDN}\Common7'.format(CP=sys.path[0],VCDN=VCDirName)
         copyDir(dirFrom,dirTo)
-        dirFrom = r'{VCPath}\..\VC'.format(VCPath=vcInstallDir)
-        dirTo = r'{currentPath}\Bin\Compiler\{ver}\VC'.format(currentPath=sys.path[0],ver=VCDirName)
+        dirFrom = r'{VCD}\..\VC'.format(VCD=vcDir)
+        dirTo = r'{CP}\Bin\Compiler\{VCDN}\VC'.format(CP=sys.path[0],VCDN=VCDirName)
         copyDir(dirFrom,dirTo)
 
 
 def getMSdkDir(ver):
-    msdir = getHKLMValue(r'SOFTWARE\Microsoft\Microsoft SDKs\Windows\{ver}'.format(ver=ver),'InstallationFolder')
+    msdir = getHKLMValue(r'SOFTWARE\Microsoft\Microsoft SDKs\Windows\{VER}'.format(VER=ver),'InstallationFolder')
     if msdir :
-        return msdir
-    msdir = getHKLMValue(r'SOFTWARE\Wow6432Node\Microsoft\Microsoft SDKs\Windows\{ver}'.format(ver=ver),'InstallationFolder')
+        return msdir.rstrip('\\')
+    msdir = getHKLMValue(r'SOFTWARE\Wow6432Node\Microsoft\Microsoft SDKs\Windows\{VER}'.format(VER=ver),'InstallationFolder')
     if msdir :
-        return msdir
+        return msdir.rstrip('\\')
     return None
 
 def copyMicrosoftSDKs(ver):
     dirFrom = getMSdkDir(ver)
     if dirFrom :
-        dirTo = +r'{currentPath}\Bin\MicrosoftSDKs\Windows\{ver}'.format(currentPath=sys.path[0],ver=ver)
+        dirTo = r'{CP}\Bin\MicrosoftSDKs\Windows\{VER}'.format(CP=sys.path[0],VER=ver)
         copyDir(dirFrom,dirTo)
 
-def copyMSBuildV110Down(VSVer,VCDirName):
-    nameFrom = r'{pf}\MSBuild\Microsoft.Cpp\v4.0\*.*'.format(pf=GetPFX86Dir())
-    dirTo = r'{currentPath}\MSBuild\{ver}'.format(currentPath=sys.path[0],ver=VCDirName)
+def copyMSBuildV110Down(VCDirName):
+    nameFrom = r'{PF}\MSBuild\Microsoft.Cpp\v4.0\*.*'.format(PF=GetPFX86Dir())
+    dirTo = r'{CP}\Bin\MSBuild\{VCDN}'.format(CP=sys.path[0],VCDN=VCDirName)
     copyFile(nameFrom,dirTo)
     
-    dirFrom = r'{pf}\MSBuild\Microsoft.Cpp\v4.0\1033'.format(pf=GetPFX86Dir())
-    dirTo = r'{currentPath}\MSBuild\{ver}\1033'.format(currentPath=sys.path[0],ver=VCDirName)
-    copyFile(nameFrom,dirTo)
-
-    dirFrom = r'{pf}\MSBuild\Microsoft.Cpp\v4.0\2052'.format(pf=GetPFX86Dir())
-    dirTo = r'{currentPath}\MSBuild\{ver}\2052'.format(currentPath=sys.path[0],ver=VCDirName)
-    copyFile(nameFrom,dirTo)
-    
-    dirFrom = r'{pf}\MSBuild\Microsoft.Cpp\v4.0\BuildCustomizations'.format(pf=GetPFX86Dir())
-    dirTo = r'{currentPath}\MSBuild\{ver}\BuildCustomizations'.format(currentPath=sys.path[0],ver=VCDirName)
-    copyFile(nameFrom,dirTo)
-
-    dirFrom = r'{pf}\MSBuild\Microsoft.Cpp\v4.0\Platforms'.format(pf=GetPFX86Dir())
-    dirTo = r'{currentPath}\MSBuild\{ver}\Platforms'.format(currentPath=sys.path[0],ver=VCDirName)
-    copyFile(nameFrom,dirTo)
-
-def copyMSBuildV110AndUp(VCDirName):
-    dirFrom = r'{pf}\MSBuild\Microsoft.Cpp\v4.0\{ver}'.format(pf=GetPFX86Dir(),ver=VCDirName)
-    dirTo = r'{currentPath}\MSBuild\{ver}'.format(currentPath=sys.path[0],ver=VCDirName)
+    dirFrom = r'{PF}\MSBuild\Microsoft.Cpp\v4.0\1033'.format(PF=GetPFX86Dir())
+    dirTo = r'{CP}\Bin\MSBuild\{VCDN}\1033'.format(CP=sys.path[0],VCDN=VCDirName)
     copyDir(dirFrom,dirTo)
 
-def getWindowsKitsDir(name):
-    wkDir = getHKLMValue(r'SOFTWARE\Wow6432Node\Microsoft\Windows Kits\Installed Roots',name)
+    dirFrom = r'{PF}\MSBuild\Microsoft.Cpp\v4.0\2052'.format(PF=GetPFX86Dir())
+    dirTo = r'{CP}\Bin\MSBuild\{VCDN}\2052'.format(CP=sys.path[0],VCDN=VCDirName)
+    copyDir(dirFrom,dirTo)
+    
+    dirFrom = r'{PF}\MSBuild\Microsoft.Cpp\v4.0\BuildCustomizations'.format(PF=GetPFX86Dir())
+    dirTo = r'{CP}\Bin\MSBuild\{VCDN}\BuildCustomizations'.format(CP=sys.path[0],VCDN=VCDirName)
+    copyDir(dirFrom,dirTo)
+
+    dirFrom = r'{PF}\MSBuild\Microsoft.Cpp\v4.0\Platforms\Itanium'.format(PF=GetPFX86Dir())
+    dirTo = r'{CP}\Bin\MSBuild\{VCDN}\Platforms\Itanium'.format(CP=sys.path[0],VCDN=VCDirName)
+    copyDir(dirFrom,dirTo)
+
+    nameFrom = r'{PF}\MSBuild\Microsoft.Cpp\v4.0\Platforms\Win32\*.*'.format(PF=GetPFX86Dir())
+    dirTo = r'{CP}\Bin\MSBuild\{VCDN}\Platforms\Win32'.format(CP=sys.path[0],VCDN=VCDirName)
+    copyFile(nameFrom,dirTo)
+
+    dirFrom = r'{PF}\MSBuild\Microsoft.Cpp\v4.0\Platforms\Win32\1033'.format(PF=GetPFX86Dir())
+    dirTo = r'{CP}\Bin\MSBuild\{VCDN}\Platforms\Win32\1033'.format(CP=sys.path[0],VCDN=VCDirName)
+    copyDir(dirFrom,dirTo)
+
+    dirFrom = r'{PF}\MSBuild\Microsoft.Cpp\v4.0\Platforms\Win32\2052'.format(PF=GetPFX86Dir())
+    dirTo = r'{CP}\Bin\MSBuild\{VCDN}\Platforms\Win32\2052'.format(CP=sys.path[0],VCDN=VCDirName)
+    copyDir(dirFrom,dirTo)
+
+    dirFrom = r'{PF}\MSBuild\Microsoft.Cpp\v4.0\Platforms\Win32\PlatformToolsets\{VCDN}'.format(PF=GetPFX86Dir(),VCDN=VCDirName)
+    dirTo = r'{CP}\Bin\MSBuild\{VCDN}\Platforms\Win32\PlatformToolsets\{VCDN}'.format(CP=sys.path[0],VCDN=VCDirName)
+    copyDir(dirFrom,dirTo)
+
+    nameFrom = r'{PF}\MSBuild\Microsoft.Cpp\v4.0\Platforms\x64\*.*'.format(PF=GetPFX86Dir())
+    dirTo = r'{CP}\Bin\MSBuild\{VCDN}\Platforms\x64'.format(CP=sys.path[0],VCDN=VCDirName)
+    copyFile(nameFrom,dirTo)
+
+    dirFrom = r'{PF}\MSBuild\Microsoft.Cpp\v4.0\Platforms\x64\1033'.format(PF=GetPFX86Dir())
+    dirTo = r'{CP}\Bin\MSBuild\{VCDN}\Platforms\x64\1033'.format(CP=sys.path[0],VCDN=VCDirName)
+    copyDir(dirFrom,dirTo)
+
+    dirFrom = r'{PF}\MSBuild\Microsoft.Cpp\v4.0\Platforms\x64\2052'.format(PF=GetPFX86Dir())
+    dirTo = r'{CP}\Bin\MSBuild\{VCDN}\Platforms\x64\2052'.format(CP=sys.path[0],VCDN=VCDirName)
+    copyDir(dirFrom,dirTo)
+
+    dirFrom = r'{PF}\MSBuild\Microsoft.Cpp\v4.0\Platforms\x64\PlatformToolsets\{VCDN}'.format(PF=GetPFX86Dir(),VCDN=VCDirName)
+    dirTo = r'{CP}\Bin\MSBuild\{VCDN}\Platforms\x64\PlatformToolsets\{VCDN}'.format(CP=sys.path[0],VCDN=VCDirName)
+    copyDir(dirFrom,dirTo)
+    
+
+def copyMSBuildV110AndUp(VCDirName):
+    dirFrom = r'{PF}\MSBuild\Microsoft.Cpp\v4.0\{VCDN}'.format(PF=GetPFX86Dir(),VCDN=VCDirName)
+    dirTo = r'{CP}\Bin\MSBuild\{VCDN}'.format(CP=sys.path[0],VCDN=VCDirName)
+    copyDir(dirFrom,dirTo)
+
+def copyMSBuild(VCDirName):
+    temp = VCDirName[1:]
+    if int(temp) > 110:
+        copyMSBuildV110AndUp(VCDirName)
+    else:
+        copyMSBuildV110Down(VCDirName)
+
+def getWindowsKitsDir(WKRegName):
+    wkDir = getHKLMValue(r'SOFTWARE\Wow6432Node\Microsoft\Windows Kits\Installed Roots',WKRegName)
     if wkDir:
-        return wkDir
-    wkDir = getHKLMValue(r'SOFTWARE\Microsoft\Windows Kits\Installed Roots',name)
+        return wkDir.rstrip('\\')
+    wkDir = getHKLMValue(r'SOFTWARE\Microsoft\Windows Kits\Installed Roots',WKRegName)
     if wkDir:
-        return wkDir
+        return wkDir.rstrip('\\')
     return None
 
-def copyWindowsKits(ver,name):
-    dirFrom = getWindowsKitsDir(name)
+def copyWindowsKits(WKVer,WKRegName):
+    dirFrom = getWindowsKitsDir(WKRegName)
     if dirFrom :
-        dirTo = r'{curpath}\Bin\MicrosoftKits\Windows\{ver}'.format(curpath=sys.path[0],ver=name)
+        dirTo = r'{CP}\Bin\MicrosoftKits\Windows\{WKV}'.format(CP=sys.path[0],WKV=WKVer)
         copyDir(dirFrom,dirTo)
 
-
-def isInstallVS(vsVersion):
-    if getVCInstallDir(vsVersion) != None :
-        return True
-    return False
-
 if __name__ == '__main__' :
-    for VSVer,VCVer,VCDirName,MSDKVer,WKVer,WKRegName,WKDir in VerList:
-        if not isInstallVS(VSVer):
+    for VSVer,VCVer,VCDirName,MSDKVer,WKVer,WKRegName in VerList:
+        if not VSVer or not VCVer:
             continue
-        if VCVer and VCDirName:
-            copyVC(VCVer,VCDirName)
+
+        if not isInstallVC(VCVer):
+            continue
+        copyVC(VCVer,VCDirName)
+
         if MSDKVer:
             copyMicrosoftSDKs(MSDKVer)
+            
         if WKVer and WKRegName:
             copyWindowsKits(WKVer,WKRegName)
         
-    #copy MSBuild
-    print('Copy MSBuild')
-    copyMSBuild
-
-    #copy WindowsKits
-    print('Copy MSBuild')
-    copyWindowsKits()
+        #copy MSBuild
+        print('Copy MSBuild')
+        copyMSBuild(VCDirName)
 
     #download vcredist
 
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # VSMultipleCompilerPlugin
 Make VS2015 Can Compiler By VC12.0, VC11.0,VC10.0,VC9.0,VC8.0, etc.
+
+
+V0.3 Fix Bug.
